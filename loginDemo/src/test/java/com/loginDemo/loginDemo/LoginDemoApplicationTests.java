6ec016a2fa933e3fe35deb295735aaa34c8b63d0
Adeel <adeel.younis07@gmail.com>
2018-03-22 18:41:10 +0500

    Added add method

--- loginDemo/src/test/java/com/loginDemo/loginDemo/LoginDemoApplicationTests.java
+++ loginDemo/src/test/java/com/loginDemo/loginDemo/LoginDemoApplicationTests.java
@@ -8,8 +8,11 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class LoginDemoApplicationTests {
+	//aaaa
 	@Test
 	public void contextLoads() {
 	}
-
+	public void addMethod(){
+		System.out.print("test");
+	}
 }
